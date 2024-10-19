928d8f7ae9dff226fa10e34e47fcd64df7730df3
meluiz <me.luizfelipe@gmail.com>
2024-10-19 20:38:49 -0300

    refactor(server.ts): remove unused server and runtimeEnv configurations to simplify environment setup

--- apps/web/lib/env/server.ts
+++ apps/web/lib/env/server.ts
@@ -6,19 +6,6 @@ import { createEnv } from '@t3-oss/env-nextjs'
 export const env = createEnv({
   isServer: typeof window === 'undefined',
   extends: [vercel()],
-  server: {
-    SESSION_SECRET: z.string(),
-    BOT_TOKEN: z.string(),
-    BOT_USER_ID: z.string(),
-    APP_URL: z
-      .string()
-      .transform((url) => new URL(url).origin)
-      .pipe(z.string().url()),
-  },
-  runtimeEnv: {
-    APP_URL: process.env.APP_URL,
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    BOT_USER_ID: process.env.BOT_USER_ID,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-  },
+  server: {},
+  runtimeEnv: {},
 })
