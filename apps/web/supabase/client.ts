8cd436594c7c3c2042876825ffd32b311ec091c4
meluiz <me.luizfelipe@gmail.com>
2024-10-19 18:36:07 -0300

    refactor(client.ts): replace createClient with createBrowserClient for SSR compatibility

--- apps/web/supabase/client.ts
+++ apps/web/supabase/client.ts
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 import { env } from 'lib/env/client'
 
 const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
