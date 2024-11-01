8fe9a141cf8b1069d05ea90d5583a2087f8711a0
meluiz <me.luizfelipe@gmail.com>
2024-10-31 22:16:21 -0300

    style(panda-preset): format JSON and TypeScript files for consistency and readability chore(panda-preset): ensure newline at end of files for better compatibility with various tools

--- packages/panda-preset/src/theme/semantic-tokens/shadows.ts
+++ packages/panda-preset/src/theme/semantic-tokens/shadows.ts
@@ -12,32 +12,37 @@ export default defineSemanticTokens.shadows({
   },
   sm: {
     value: {
-      _light: `0 1px 2px 0 {colors.gray.50/5}`,
-      _dark: `0 1px 2px 0 {colors.black/5}`,
+      _light: '0 1px 2px 0 {colors.gray.50/5}',
+      _dark: '0 1px 2px 0 {colors.black/5}',
     },
   },
   md: {
     value: {
-      _light: `0 4px 6px -1px {colors.gray.50/5}, 0 2px 4px -2px {colors.gray.50/5}`,
-      _dark: `0 4px 6px -1px {colors.black/5}, 0 2px 4px -2px {colors.black/5}`,
+      _light:
+        '0 4px 6px -1px {colors.gray.50/5}, 0 2px 4px -2px {colors.gray.50/5}',
+      _dark: '0 4px 6px -1px {colors.black/5}, 0 2px 4px -2px {colors.black/5}',
     },
   },
   lg: {
     value: {
-      _light: `0 10px 15px -3px {colors.gray.50/5}, 0 4px 6px -4px {colors.gray.50/5}`,
-      _dark: `0 10px 15px -3px {colors.black/5}, 0 4px 6px -4px {colors.black/5}`,
+      _light:
+        '0 10px 15px -3px {colors.gray.50/5}, 0 4px 6px -4px {colors.gray.50/5}',
+      _dark:
+        '0 10px 15px -3px {colors.black/5}, 0 4px 6px -4px {colors.black/5}',
     },
   },
   xl: {
     value: {
-      _light: `0 20px 25px -5px {colors.gray.50/5}, 0 10px 10px -6px {colors.gray.50/5}`,
-      _dark: `0 20px 25px -5px {colors.black/5}, 0 10px 10px -6px {colors.black/5}`,
+      _light:
+        '0 20px 25px -5px {colors.gray.50/5}, 0 10px 10px -6px {colors.gray.50/5}',
+      _dark:
+        '0 20px 25px -5px {colors.black/5}, 0 10px 10px -6px {colors.black/5}',
     },
   },
   '2xl': {
     value: {
-      _light: `0 25px 50px -12px {colors.gray.50/5}`,
-      _dark: `0 25px 50px -12px {colors.black/5}`,
+      _light: '0 25px 50px -12px {colors.gray.50/5}',
+      _dark: '0 25px 50px -12px {colors.black/5}',
     },
   },
 })
