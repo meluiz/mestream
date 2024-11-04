a98c04f13013d5eafe7c1ca56482243a5987158d
meluiz <me.luizfelipe@gmail.com>
2024-11-04 18:43:29 -0300

    feat(ui): add new UI components including Anchor, Avatar, Heading, Img, Menu, and Text to enhance the frontend design system

---
+++ apps/frontend/src/components/ui/anchor/index.ts
@@ -0,0 +1,32 @@
+'use client'
+import type React from 'react'
+
+import Link from 'next/link'
+import { type StyledComponent, styled } from 'pandacss/jsx'
+
+type StyledProps = React.ComponentProps<typeof Styled> & {
+  as?: React.ElementType
+}
+
+// @ts-ignore
+const Styled = styled(
+  Link,
+  {
+    base: {
+      objectFit: 'cover',
+      userSelect: 'none',
+      objectPosition: 'center',
+    },
+  },
+  {
+    defaultProps: {
+      role: 'link',
+    },
+    shouldForwardProp: (props) => {
+      return ['href'].includes(props)
+    },
+  },
+)
+
+export type AnchorProps = StyledComponent<'a', StyledProps>
+export const Anchor = Styled as AnchorProps
---
+++ apps/frontend/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './avatar'
+export * from './heading'
+export * from './text'
+export * from './img'
+export * from './anchor'
+export * from './menu'
+export * from './anchor'
